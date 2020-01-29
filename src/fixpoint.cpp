a84b52aa3eda07fd8144cdea820c08132f931547
Tim Gymnich <tg908@icloud.com>
2020-01-29 22:34:27 +0100

    Implemented some hash functions

--- src/fixpoint.cpp
+++ src/fixpoint.cpp
@@ -20,6 +20,44 @@
 #include "fixpoint_two_var_eq.cpp"
 
 
+namespace std {
+
+template <typename ... T>
+struct hash<tuple<T...>> {
+    size_t operator()(tuple<T...> const& t) const {
+        size_t seed = 0;
+        apply([&seed](const auto&... item) {(( seed = llvm::hash_combine(seed, item) ), ...);}, t);
+        return seed;
+    }
+};
+
+template <typename T, typename U>
+struct hash<pair<T, U>> {
+    size_t operator()(pair<T, U> const& in) const {
+        return llvm::hash_value(in);
+    }
+};
+
+
+template <typename T>
+struct hash<vector<T>> {
+    size_t operator()(vector<T> const& in) const {
+        return llvm::hash_combine_range(in.begin(), in.end());
+    }
+};
+
+}
+
+namespace llvm {
+
+template <typename T>
+static hash_code hash_value(std::vector<T> const& in) {
+    return hash_combine_range(in.begin(), in.end());
+}
+
+}
+
+
 namespace pcpo {
 
 using namespace llvm;
