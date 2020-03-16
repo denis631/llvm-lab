7eb7a208db64bac27675c8d06e5ddd26ea2139fc
Tim Gymnich <tg908@icloud.com>
2020-03-16 02:54:22 +0100

    Added tests for affine relation

---
+++ test/affine_relation_test.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <cstdint>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "../src/affine_relation.h"
+
+using namespace std;
+using namespace pcpo;
+
+template <typename T>
+class AffineRelationTest: AffineRelation<T> {
+
+public:
+
+};
+
+
+int main() {
+    return true;
+};
+
+
